0a32a14b4855fe366b322dffc230021fd560f3bc
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-29 17:57:12 +0530

    Add Header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import PropTypes from 'prop-types';
+
+
+const Header = ({title}) => {
+  return (
+    <header>
+      <h1 onClick = {() => window.location.reload(false)}>{title}</h1>
+    </header>
+  );
+};
+
+Header.defaultProps = {title: 'Bored?'};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
