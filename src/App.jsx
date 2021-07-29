cb2305a95f2d94a7d9a0379bb146b5c1aceb0dc9
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-07-29 17:50:56 +0530

    Add App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Bored from './components/bored/Bored.jsx';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Bored/>
+    </div>
+  );
+};
+
+export default App;
