e5e5e2626c6869cb9d5bb92e87779235c99b4f98
Abhishek Keshri <iam2kabhishek@gmail.com>
2021-12-22 16:12:10 +0530

    Show loading while fetching data.

--- src/components/bored/Bored.jsx
+++ src/components/bored/Bored.jsx
@@ -2,30 +2,31 @@ import React from 'react';
 import axios from 'axios';
 import './Bored.css';
 import Header from '../header/Header';
-import Button from '../button/Button';
+import Activity from '../activity/Activity';
 
 const baseURL = `https://www.boredapi.com/api/activity`;
 
 const Bored = () => {
   const [activity, setActivity] = React.useState('');
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      console.log(response.data);
-      setActivity(response.data);
-    }).catch((error) => {
-      console.log(error);
-    });
+    axios
+        .get(baseURL)
+        .then((response) => {
+          console.log(response.data);
+          setActivity(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
   }, []);
 
   return (
-    <div className="Bored">
-      <Header title='Bored?'/>
-      <h2>{activity.activity}</h2>
-      <h3>{activity.type}</h3>
-      {activity.link.length > 0 ?
-      (<Button text='Get Started!' link={activity.link.toString()}/>) : null
-      }
+    <div className='Bored'>
+      <Header title='Bored?' />
+      {loading ? <h2>Loading...</h2> : <Activity activity={activity} />}
     </div>
   );
 };
